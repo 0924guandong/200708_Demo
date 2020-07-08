432f2e92be60d498dd7248dc8e180e00811a0947
0924guandong <34070087@qq.com>
2020-07-08 22:24:36 +0800

    初始化导入数据

---
+++ src/main/java/com/example/git_pull/GitPullApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_pull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_pull/GitPullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_pull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
